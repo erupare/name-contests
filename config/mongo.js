e389ae3912634d6c688ec6fdd417b12646b051ef
Edgar Rupare <erupar200@caledonian.ac.uk>
2019-05-18 00:36:24 +0100

    updated m3-02

--- config/mongo.js
+++ config/mongo.js
@@ -1,5 +1,12 @@
 module.exports = {
   development: {
     url: 'mongodb://localhost:27017/contests'
+    // url: 'mongodb+srv://eddie:test1234@graphql-cluster-l4enk.mongodb.net/graphql-cluster?retryWrites=true'
+    // url: 'mongodb+srv://eddie:test1234@graphql-cluster-l4enk.mongodb.net/contests?retryWrites=true'
+    // url: 'mongodb+srv://gql:test1234@graphql-cluster-l4enk.mongodb.net/contests'
+    // url: 'mongodb+srv://eddie:test1234@graphql-cluster-l4enk.mongodb.net/gql-erupar?retryWrites=true'
+
+    // mongodb+srv://eddie:test1234@graphql-cluster-l4enk.mongodb.net/gql-erupar?retryWrites=true
+
   }
 };
